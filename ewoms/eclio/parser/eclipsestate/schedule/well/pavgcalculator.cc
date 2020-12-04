86b6f482edd1ba9e172b177dcc424b41800e184f
Andreas Lauser <and@poware.org>
2020-12-04 16:27:23 +0100

    only print annoying "PORV based averaging is not yet supported in WBPx" warning once
    instead of about a dozen per timestep. (Norne was affected by this.)

--- ewoms/eclio/parser/eclipsestate/schedule/well/pavgcalculator.cc
+++ ewoms/eclio/parser/eclipsestate/schedule/well/pavgcalculator.cc
@@ -57,9 +57,14 @@ PAvgCalculator::PAvgCalculator(const std::string& well, const EclipseGrid& grid,
     well_name(well),
     m_pavg(pavg)
 {
-    if (pavg.use_porv())
-        OpmLog::warning("PORV based averaging is not yet supported in WBPx");
+    if (pavg.use_porv()) {
+        static bool warningPrinted = false;
+        if (!warningPrinted) {
+            OpmLog::warning("PORV based averaging is not yet supported in WBPx");
+            warningPrinted = true;
+        }
         //throw std::logic_error("The current implementation does not yet support PORV based averaging");
+    }
 
     for (const auto& conn : connections) {
         if (conn.state() == ::Ewoms::Connection::State::OPEN || !this->m_pavg.open_connections()) {
